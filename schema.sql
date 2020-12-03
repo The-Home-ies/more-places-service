82a3351bb7d0825ace47539a3060bed66be785ed
jwu23 <jasonwu1623@gmail.com>
2020-12-02 23:25:17 -0800

    Initial schema design for psql

---
+++ schema.sql
@@ -0,0 +1,42 @@
+CREATE TABLE listings (
+  id SERIAL NOT NULL,
+  listing_name VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id)
+  -- more_places_id SERIAL NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE more_places (
+  id INT NOT NULL,
+  listing_id INT NOT NULL,
+  picture_url VARCHAR(200) NOT NULL,
+  location_name VARCHAR(100) NOT NULL,
+  liked BOOLEAN DEFAULT FALSE,
+  score INT NOT NULL,
+  review_count INT NOT NULL,
+  room_type VARCHAR(50) NOT NULL,
+  room_name VARCHAR(50) NOT NULL,
+  bed_count INT NOT NULL,
+  cost_per_night INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (listing_id) REFERENCES listings(id)
+);
+
+INSERT INTO listings (
+  listing_name
+) VALUES ('Listing one');
+
+INSERT INTO more_places (
+  id,
+  listing_id,
+  picture_url,
+  location_name,
+  liked,
+  score,
+  review_count,
+  room_type,
+  room_name,
+  bed_count,
+  cost_per_night
+) VALUES (12, 1, 'urlhere', 'location_name', FALSE, 4, 42, 'roomType', 'room name', 3, 312);
+
+SELECT * FROM more_places INNER JOIN listings ON (more_places.listing_id = listings.id) WHERE listings.id = 1;
