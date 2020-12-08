fc616f8a639cc4170c90d975840fcc42ad07cf1e
jwu23 <jasonwu1623@gmail.com>
2020-12-07 18:28:54 -0800

    Small fixes to schema

--- sdcSchemas/schema.sql
+++ sdcSchemas/schema.sql
@@ -1,7 +1,9 @@
 DROP DATABASE IF EXISTS places;
 CREATE DATABASE places;
 
-DROP TABLE IF EXISTS listings;
+\c places
+
+DROP TABLE IF EXISTS listings CASCADE;
 CREATE TABLE listings (
   id SERIAL,
   listing_name VARCHAR(100) NOT NULL,
@@ -13,31 +15,31 @@ CREATE TABLE listings (
   room_type VARCHAR(100) NOT NULL,
   room_name VARCHAR(100) NOT NULL,
   bed_count INT NOT NULL,
-  cost_per_night INT NOT NULL,
-  PRIMARY KEY (id)
+  cost_per_night INT NOT NULL
+  -- PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS more_places;
 CREATE TABLE more_places (
   listing_id INT,
   suggested_id INT,
-  ranking INT,
-  FOREIGN KEY (listing_id) REFERENCES listings(id),
-  FOREIGN KEY (suggested_id) REFERENCES listings(id)
+  ranking VARCHAR(20) NOT NULL
+  -- FOREIGN KEY (listing_id) REFERENCES listings(id),
+  -- FOREIGN KEY (suggested_id) REFERENCES listings(id)
 );
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   user_id SERIAL,
-  username VARCHAR(25) NOT NULL,
-  favorites INT,
-  PRIMARY KEY (user_id)
+  username VARCHAR(100) NOT NULL,
+  favorites INT
+  -- PRIMARY KEY (user_id)
 );
 
 DROP TABLE IF EXISTS favorites;
 CREATE TABLE favorites (
   user_id INT,
-  favorite_id INT,
-  FOREIGN KEY (user_id) REFERENCES users(user_id),
-  FOREIGN KEY (favorite_id) REFERENCES listings(id)
+  favorite_id INT
+  -- FOREIGN KEY (user_id) REFERENCES users(user_id),
+  -- FOREIGN KEY (favorite_id) REFERENCES listings(id)
 );
