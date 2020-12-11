c1ceccb2b75f36c3ec44490e358514f6aac57eb0
jwu23 <jasonwu1623@gmail.com>
2020-12-11 14:57:37 -0800

    Cleaned up a bit

--- sdcSchemas/schema.sql
+++ sdcSchemas/schema.sql
@@ -8,12 +8,9 @@ CREATE TABLE listings (
   id SERIAL,
   listing_name VARCHAR(100) NOT NULL,
   picture_url VARCHAR(200) NOT NULL,
-  -- location_name VARCHAR(150) NOT NULL,
-  -- liked BOOLEAN,
   score VARCHAR(5) NOT NULL,
   review_count INT NOT NULL,
   room_type VARCHAR(100) NOT NULL,
-  -- room_name VARCHAR(100) NOT NULL,
   bed_count INT NOT NULL,
   cost_per_night INT NOT NULL
   -- PRIMARY KEY (id)
