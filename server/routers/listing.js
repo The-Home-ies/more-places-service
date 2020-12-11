7770e10bfc480c0237a7bff30dfab17b6a4ad2ed
jwu23 <jasonwu1623@gmail.com>
2020-12-10 23:28:55 -0800

    Add api routes

--- server/routers/listing.js
+++ server/routers/listing.js
@@ -1,19 +1,62 @@
 const express = require('express');
-const listingController = require('../../db/controllers/listing.js');
+const listingController = require('../../db/sdcDb/sdcController.js');
 
 // create a route handler
 const router = express.Router();
 
-router.route('/api/:id/places')
+router.route('/api/listing/:id/places')
   .get((req, res) => {
     const { id } = req.params;
-    listingController.findOne(id, (err, data) => {
+    listingController.getListingById(id, (err, data) => {
       if (err) {
         console.log(err);
+        res.status(404);
       } else {
         res.status(200).json(data);
       }
-    });
+    })
   });
+
+router.route('/api/listing/:id/places')
+  .post((req, res) => {
+    console.log('post request')
+    const body = req.body;
+    listingController.createListing(body, (err) => {
+      if (err) {
+        console.log(err);
+        res.status(404);
+      } else {
+        res.status(200);
+      }
+    })
+  })
+
+router.route('/api/listing/:id/places')
+  .put((req, res) => {
+    console.log('time to update');
+    const { id } = req.params;
+    listingController.updateListing(id, (err) => {
+      if (err) {
+        console.log(err);
+        res.status(404);
+      } else {
+        res.status(200);
+      }
+    })
+  })
+
+router.route('/api/listing/:id/places')
+  .delete((req, res) => {
+    console.log('time to delete');
+    const {id} = req.params;
+    listingController.removeListing(id, (err) => {
+      if (err) {
+        console.log(err);
+        res.status(404);
+      } else {
+        res.status(200);
+      }
+    })
+  })
 
 module.exports = router;
