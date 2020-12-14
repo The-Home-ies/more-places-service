1069c43f306dea92e76c19602f9e3223c0963975
jwu23 <jasonwu1623@gmail.com>
2020-12-13 17:20:27 -0800

    API routes

--- db/sdcDb/sdcController.js
+++ db/sdcDb/sdcController.js
@@ -1,13 +1,35 @@
-const pool = require('./sdcPool.js');
+// const pool = require('./sdcPool.js');
+
+const {Pool} = require('pg');
+
+const pool = new Pool({
+  user: 'jason',
+  host: 'localhost',
+  database: 'places',
+  port: 5432
+});
+
+// pool.on('error', (err, client) => {
+//   console.log('Error:', err);
+// });
 
 
 const getListingById = (id, callback) => {
+  pool.query(`SELECT l.*, m.ranking FROM listings l LEFT JOIN more_places m ON (l.id = m.suggested_id) WHERE listing_id = ${id};`, (err, res) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, res.rows);
+    }
+  })
+}
+
+const createListing = (body, callback) => {
   pool.connect()
     .then((client) => {
-      client.query(`SELECT l.*, m.ranking, m.suggested_id FROM listings l JOIN more_places m ON (l.id = m.listing_id) WHERE id = ${id};`)
+      client.query(`INSERT INTO more_places(listing_id, suggested_id, ranking) VALUES (${body.listing_id}, ${body.suggested_id}, ${body.ranking});`)
         .then(res => {
-          console.log('get request');
-          callback(null, res.rows)
+          callback(null)
           client.release();
         })
         .catch(err => {
@@ -16,23 +38,17 @@ const getListingById = (id, callback) => {
           client.release();
         })
     })
-}
-
-const createListing = (body, callback) => {
-  console.log('controller',body)
+    .catch(err => {
+      console.log(err);
+    })
 }
 
 const updateListing = (body, callback) => {
   console.log('want to update listing');
-}
-
-const removeListing = (id, callback) => {
-  console.log('want to delete ', id);
   pool.connect()
     .then((client) => {
-      client.query(``)
-        .then (res => {
-          console.log('delete request');
+      client.query(`UPDATE more_places SET ranking = ${body.ranking} WHERE listing_id = ${body.listing_id} AND suggested_id = ${body.suggested_id};`)
+        .then(res => {
           callback(null);
           client.release();
         })
@@ -41,6 +57,28 @@ const removeListing = (id, callback) => {
           callback(err);
           client.release();
         })
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
+
+const removeListing = (body, callback) => {
+  pool.connect()
+    .then((client) => {
+      client.query(`DELETE FROM more_places WHERE listing_id = ${body.listing_id} AND suggested_id = ${body.suggested_id};`)
+        .then(res => {
+          callback(null);
+          client.release();
+        })
+        .catch(err => {
+          console.log(err);
+          callback(err);
+          client.release();
+        })
+    })
+    .catch(err => {
+      console.log(err);
     })
 }
 
