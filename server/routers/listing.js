8f92e6dacb42d33ce055d80359b20286a07ace17
jwu23 <jasonwu1623@gmail.com>
2020-12-13 17:22:17 -0800

    API routes

--- server/routers/listing.js
+++ server/routers/listing.js
@@ -4,57 +4,59 @@ const listingController = require('../../db/sdcDb/sdcController.js');
 // create a route handler
 const router = express.Router();
 
-router.route('/api/listing/:id/places')
+router.route('/api/:id/places')
   .get((req, res) => {
     const { id } = req.params;
     listingController.getListingById(id, (err, data) => {
       if (err) {
         console.log(err);
-        res.status(404);
+        res.sendStatus(404);
       } else {
         res.status(200).json(data);
       }
     })
   });
 
-router.route('/api/listing/:id/places')
+router.route('/api/listing/places')
   .post((req, res) => {
     console.log('post request')
     const body = req.body;
     listingController.createListing(body, (err) => {
       if (err) {
         console.log(err);
-        res.status(404);
+        res.sendStatus(404);
       } else {
-        res.status(200);
+        res.sendStatus(200);
       }
     })
   })
 
-router.route('/api/listing/:id/places')
+router.route('/api/listing/places')
   .put((req, res) => {
     console.log('time to update');
-    const { id } = req.params;
-    listingController.updateListing(id, (err) => {
+    // const { id } = req.params;
+    // console.log(id)
+    const body = req.body;
+    listingController.updateListing(body, (err) => {
       if (err) {
         console.log(err);
-        res.status(404);
+        res.sendStatus(404);
       } else {
-        res.status(200);
+        res.sendStatus(200);
       }
     })
   })
 
-router.route('/api/listing/:id/places')
+router.route('/api/listing/places')
   .delete((req, res) => {
     console.log('time to delete');
-    const {id} = req.params;
-    listingController.removeListing(id, (err) => {
+    const body = req.body
+    listingController.removeListing(body, (err) => {
       if (err) {
         console.log(err);
-        res.status(404);
+        res.sendStatus(404);
       } else {
-        res.status(200);
+        res.sendStatus(200);
       }
     })
   })
