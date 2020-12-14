ecd1b5dd2de1353590e814def85bc0b2333a14da
jwu23 <jasonwu1623@gmail.com>
2020-12-13 17:23:19 -0800

    API routes

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ const app = require('./app.js');
 const PORT = 3004;
 
 // connect to the listings database
-mongoose.connect('mongodb://localhost/listings');
 
 app.listen(PORT, () => {
   console.log(`more places module listening on http://localhost/${PORT}`);
